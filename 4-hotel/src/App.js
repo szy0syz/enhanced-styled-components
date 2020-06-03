f36b2edb4ba548b9b77d22675792720360540cbb
szy0syz <szy0syz@gmail.com>
2020-06-03 14:41:32 +0800

    Add GlobalStyle

--- 4-hotel/src/App.js
+++ 4-hotel/src/App.js
@@ -1,9 +1,11 @@
 import React, { Fragment } from 'react';
 import Home from './pages/Home'
+import Globals from './components/globals/GlobalStyles'
 
 function App() {
   return (
     <Fragment>
+      <Globals />
       <Home />
     </Fragment>
   );
